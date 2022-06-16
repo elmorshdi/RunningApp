a4e01efee9555eb0162a4d19d6af98689d9db94a
elmorshdi53 <elmorshdi53@std.mans.edu.eg>
2022-06-16 12:35:55 +0200

    release

--- app/src/main/java/com/elmorshdi/runningapp/db/RunDao.kt
+++ app/src/main/java/com/elmorshdi/runningapp/db/RunDao.kt
@@ -8,8 +8,6 @@ interface RunDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRun(run: Run)
 
-    @Delete
-    suspend fun deleteRun(run: Run)
 
     @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
     fun getAllRunsSortedByDate(): LiveData<List<Run>>
